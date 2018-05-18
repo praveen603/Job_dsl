a607716767590a5c8ecbcde451a9448280c39ea3
praveen603 <v.praveenchinna@gmail.com>
2018-05-17 19:23:22 -0700

    Update seed.groovy

--- jenkins/jobs/seed.groovy
+++ jenkins/jobs/seed.groovy
@@ -1,4 +1,4 @@
-pipelineJob('javabuild') {  
+pipeline('javabuild') {  
     description ''' <b>This job is managed by the devops in <code>jenkins/jobs/pipeline/javabuild.groovy</code>, 
     DO NOT MODIFY</b>   '''
     logRotator 30 
